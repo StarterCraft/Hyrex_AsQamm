76ada3c9c12b8c8d8060cbebc9f2b358e3d4ba3c
StarterCraft <vladislavgolubevirk@gmail.com>
2021-02-20 16:58:25 +0800

    Добавлено программное ограничение на частоту запросов DHT

--- AsQammArduino/Features/AsQammArduinoFeature_DHTNew.h
+++ AsQammArduino/Features/AsQammArduinoFeature_DHTNew.h
@@ -10,61 +10,61 @@ void DHTt(const uint8_t pin) {
     char result[16], catres[8];
     boolean success = false;
     
-    int chk = sensor.read();
-
-    switch (chk) {
-    case DHTLIB_OK:
-        strcpy(result, "OK;");
-        success = true;
-        break;
-    case DHTLIB_ERROR_CHECKSUM:
-        Firmata.sendString("ERR;DHTt;CHKS");
-        break;
-    case DHTLIB_ERROR_TIMEOUT_A:
-        Firmata.sendString("ERR;DHTt;TIMA");
-        break;
-    case DHTLIB_ERROR_TIMEOUT_B:
-        Firmata.sendString("ERR;DHTt;TIMB");
-        break;
-    case DHTLIB_ERROR_TIMEOUT_C:
-        Firmata.sendString("ERR;DHTt;TIMC");
-        break;
-    case DHTLIB_ERROR_TIMEOUT_D:
-        Firmata.sendString("ERR;DHTt;TIMD");
-        break;
-    case DHTLIB_ERROR_SENSOR_NOT_READY:
-        Firmata.sendString("ERR;DHTt;SENR");
-        break;
-    case DHTLIB_ERROR_BIT_SHIFT:
-        Firmata.sendString("ERR;DHTt;BITS");
-        break;
-    case DHTLIB_WAITING_FOR_READ:
-        Firmata.sendString("ERR;DHTt;WAFR");
-        break;
-    default:
-        strcpy(result, "ERR;DHTt;UNKN;");
-        itoa(chk, catres, 10);
-        strcat(result, catres);
-        Firmata.sendString(result);
-        break;
-    }
-
-    if (success) {
+    if ((millis() - readingTime) > 2000) {
         readingTime = millis();
-        dtostrf(sensor.getTemperature(), 5, 1, catres);
-        strcat(result, catres);
-        strcat(result, ";");
+        int chk = sensor.read();
 
-        dtostrf(sensor.getHumidity(), 5, 1, catres);
-        strcat(result, catres);
-
-        Firmata.sendString(result);
-
-        while ((millis() - readingTime) < 2000) {
-            ;;
+        switch (chk) {
+        case DHTLIB_OK:
+            strcpy(result, "OK;");
+            success = true;
+            break;
+        case DHTLIB_ERROR_CHECKSUM:
+            Firmata.sendString("ERR;DHTt;CHKS");
+            break;
+        case DHTLIB_ERROR_TIMEOUT_A:
+            Firmata.sendString("ERR;DHTt;TIMA");
+            break;
+        case DHTLIB_ERROR_TIMEOUT_B:
+            Firmata.sendString("ERR;DHTt;TIMB");
+            break;
+        case DHTLIB_ERROR_TIMEOUT_C:
+            Firmata.sendString("ERR;DHTt;TIMC");
+            break;
+        case DHTLIB_ERROR_TIMEOUT_D:
+            Firmata.sendString("ERR;DHTt;TIMD");
+            break;
+        case DHTLIB_ERROR_SENSOR_NOT_READY:
+            Firmata.sendString("ERR;DHTt;SENR");
+            break;
+        case DHTLIB_ERROR_BIT_SHIFT:
+            Firmata.sendString("ERR;DHTt;BITS");
+            break;
+        case DHTLIB_WAITING_FOR_READ:
+            Firmata.sendString("ERR;DHTt;WAFR");
+            break;
+        default:
+            strcpy(result, "ERR;DHTt;UNKN;");
+            itoa(chk, catres, 10);
+            strcat(result, catres);
+            Firmata.sendString(result);
+            break;
         }
 
-        return;
+        if (success) {
+            readingTime = millis();
+            dtostrf(sensor.getTemperature(), 5, 1, catres);
+            strcat(result, catres);
+            strcat(result, ";");
 
-    } else { return; }
+            dtostrf(sensor.getHumidity(), 5, 1, catres);
+            strcat(result, catres);
+
+            Firmata.sendString(result);
+            return;
+
+        } else {
+            return; 
+        }
+    }
 }
